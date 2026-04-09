535020d4ac75f96609cec10b5159f4ec5dd4fa14
Joshua Dave Azurin <jdeeza@Joshuas-MacBook-Air.local>
2026-04-09 16:37:18 +0800

    actually redirects when found

--- app/routers/url_finder.py
+++ app/routers/url_finder.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter,Depends
 from ..database import get_db
 from sqlalchemy.orm import Session
 from app import models
+from fastapi.responses import RedirectResponse
 
 router = APIRouter()
 
@@ -9,5 +10,5 @@ router = APIRouter()
 def url_finder(short_link: str, db : Session = Depends(get_db)):
     query = db.query(models.urlsConverted).filter(models.urlsConverted.short_URL == short_link).first()
     if query:
-        return {"message" : "found"}
+        return RedirectResponse(url=query.long_URL)  # type: ignore
     raise Exception
