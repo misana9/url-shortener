9d227f1180f931589c8df6c12647ee579b10e7b5
Joshua Dave Azurin <jdeeza@Joshuas-MacBook-Air.local>
2026-04-09 16:37:59 +0800

    added refresh for every query, only returns the link when url is already shortened

--- app/routers/url_shortener.py
+++ app/routers/url_shortener.py
@@ -13,8 +13,9 @@ router = APIRouter(
 def url_shortener(url : schemas.urlFormat, db: Session = Depends(database.get_db), short_link = utils.generate_short_code()):
     query = db.query(models.urlsConverted).filter(models.urlsConverted.long_URL == url.long_URL).first()
     if query:
-        return query
+        return query.long_URL
     new_link = models.urlsConverted(short_URL = short_link, **url.model_dump())
     db.add(new_link)
     db.commit()
+    db.refresh(new_link)
     return {"message": "done"}
