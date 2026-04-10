a5778da3ddf20b1c4ea21030ca300e1ce4aafbd1
Joshua Dave Azurin <jdeeza@Joshuas-MacBook-Air.local>
2026-04-10 16:37:41 +0800

    set variables for jwt coding/decoding

--- app/config.py
+++ app/config.py
@@ -6,6 +6,8 @@ class Settings(BaseSettings):
     database_password:str
     database_name:str
     database_username:str
+    secret_key: str
+    algorithm: str
 
     class Config:
         env_file = ".env"
