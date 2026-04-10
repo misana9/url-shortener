e8491efba4acb138dab75ebe31266fd222e10ea2
Joshua Dave Azurin <jdeeza@Joshuas-MacBook-Air.local>
2026-04-10 16:39:08 +0800

    set util functions for password hashing and verifying

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,17 @@
 import random
 import string
+from pwdlib import PasswordHash
 
 def generate_short_code(length=6):
     characters = string.ascii_letters + string.digits
     return ''.join(random.choices(characters, k=length))
+
+password_hash = PasswordHash.recommended()
+
+def hash_password(plain:str):
+    return password_hash.hash(plain)
+
+
+def verify_password(plain:str,hashed:str):
+    return password_hash.verify(plain,hashed)
+
