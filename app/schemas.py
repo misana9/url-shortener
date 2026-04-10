de6f9d0c8381539fbdff8a7898f8328cdc3c2364
Joshua Dave Azurin <jdeeza@Joshuas-MacBook-Air.local>
2026-04-10 16:38:34 +0800

    set schemas for tokens

--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,16 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class urlFormat(BaseModel):
     long_URL : str
+
+class user(BaseModel):
+    email : str
+    password : str
+
+class Token(BaseModel):
+    access_token : str
+    token_type : str
+
+class tokenData(BaseModel):
+    id : Optional[int] = None
