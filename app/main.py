ac47d4b331ab6d82c614bf33ad03c64ec5068687
Joshua Dave Azurin <jdeeza@Joshuas-MacBook-Air.local>
2026-04-10 16:39:26 +0800

    set router for new routers

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
-from .routers import url_shortener,url_finder
+from .routers import url_shortener,url_finder,users,auth
 
 
 app = FastAPI()
 
+app.include_router(users.router)
 app.include_router(url_shortener.router)
 app.include_router(url_finder.router)
+app.include_router(auth.router)
+
 
 @app.get("/")
 async def root():
