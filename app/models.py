f1251778b41651ad5afedf7f4c47b8d2f0843079
misana9 <once.jd2@gmail.com>
2026-04-11 15:36:07 +0800

    alembic tables for owner id and created at

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from .database import Base
-from sqlalchemy import Column,Integer,String
+from sqlalchemy import Column,Integer,String,ForeignKey
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.sql.expression import text
 
 class urlsConverted(Base):
     __tablename__ = "URLS"
@@ -7,6 +9,8 @@ class urlsConverted(Base):
     id = Column(Integer, primary_key=True,nullable=False)
     long_URL = Column(String,nullable=False)
     short_URL = Column(String,nullable=False)
+    owner_id = Column(Integer,ForeignKey("users.id", ondelete="CASCADE"),nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True),nullable=False,server_default=text('now()'))
 
 class Users(Base):
     __tablename__ = "users"
