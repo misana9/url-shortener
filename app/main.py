75d08efc0558ef79e954e8a90f3af291e137ec27
misana9 <once.jd2@gmail.com>
2026-04-14 14:24:17 +0800

    mounted frontend to main

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from .routers import url_shortener,url_finder,users,auth
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+
 
 app = FastAPI()
 
@@ -20,6 +23,12 @@ app.include_router(users.router)
 app.include_router(url_shortener.router)
 app.include_router(url_finder.router)
 app.include_router(auth.router)
+
+app.mount("/static", StaticFiles(directory="frontend"), name="static")
+
+@app.get("/")
+def index():
+    return HTMLResponse(open("frontend/index.html").read())
 
 
 @app.get("/")
